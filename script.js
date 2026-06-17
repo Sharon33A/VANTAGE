01409064733a70c55184a2ad3253940334f8a02f
Sharon Angel <160038769+Sharon33A@users.noreply.github.com>
2026-06-17 18:15:25 +0530

    Initial FutureMirror prototype

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("startBtn");
+
+button.addEventListener("click", () => {
+    alert("Welcome to FutureMirror!");
+    console.log("FutureMirror Loaded Successfully");
+});
